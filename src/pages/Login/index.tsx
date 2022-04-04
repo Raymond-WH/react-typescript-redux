4f47a174d9619f340ee249bbfd3f37992085ec9f
水木年华 <2461981896@qq.com>
2022-04-04 21:10:11 +0800

    feat:登录表单基本搭建与验证+axios+redux初始化

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,52 @@
-import {Button } from 'antd-mobile'
-const Login = () => {
+import styles from './index.module.scss'
+import { NavBar, Form, Input, List, Button } from 'antd-mobile'
+import { useHistory } from 'react-router'
+export default function Login() {
+  const history = useHistory()
+  const onFinish = (values: any) => { 
+    console.log(values);
+    
+  }
   return (
-    <div>
-      登录页面
-      <div>
-        <Button color="primary">我是个按钮</Button>
+    <div className={styles.root}>
+      <NavBar onBack={() => history.go(-1)}></NavBar>
+
+      {/* 表单 */}
+      <div className="login-form">
+        <h2 className="title">账号登录</h2>
+        {/* 失去焦点的时候以及改变的时候触发校验 */}
+        <Form validateTrigger={['onChange', 'onBlur']} onFinish={ onFinish}>
+          <Form.Item className="login-item" name="mobile" rules={[
+            {
+            required: true,
+            message:'手机号不能为空'
+            },
+            {
+              pattern: /^1[3456789]\d{9}$/,
+              message: '手机号格式不正确'
+            }
+          ]}>
+            <Input placeholder="请输入用户名" autoComplete='off'></Input>
+          </Form.Item>
+          <List.Item
+            className="login-code-extra"
+            extra={<span className="code-extra">发送验证码</span>}
+          >
+            <Form.Item className="login-item" name="code" rules={[
+              { required: true, message: '验证码不能为空' },
+              {pattern: /^\d{6}$/, message: '验证码格式不正确'}
+            ]}>
+              <Input placeholder="请输入验证码" autoComplete='off'></Input>
+            </Form.Item>
+          </List.Item>
+          <Form.Item>
+            {/* 提交表单校验 */}
+            <Button color="primary" type='submit' block className="login-submit">
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
       </div>
     </div>
   )
 }
-export default Login
---
+++ src/store/reducers/login.ts
@@ -0,0 +1,7 @@
+const initialState = {}
+
+const login = (state = initialState, action: any) => {
+  return state
+}
+
+export default login

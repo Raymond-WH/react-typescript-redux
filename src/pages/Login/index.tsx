b2a0956dd70db7766c6fb8fc3f863b9dd5944308
水木年华 <2461981896@qq.com>
2022-04-05 09:18:44 +0800

    feat:错误提示统一处理

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,14 +1,21 @@
 import styles from './index.module.scss'
-import { NavBar, Form, Input, List, Button } from 'antd-mobile'
+import { NavBar, Form, Input, List, Button, Toast } from 'antd-mobile'
 import { useHistory } from 'react-router'
 import { useDispatch } from 'react-redux'
 import { Loginform } from '@/types/data'
 import { login } from '@/store/actions/login'
+
 export default function Login() {
   const history = useHistory()
   const dispatch = useDispatch()
-  const onFinish = (values: Loginform) => { 
-    dispatch(login(values))
+  const onFinish = async(values: Loginform) => {
+    await dispatch(login(values))
+    Toast.show({
+      content: '登录成功',
+      icon: 'success',
+    })
+    // 跳转到首页
+    history.push('/home')
   }
   return (
     <div className={styles.root}>
@@ -18,33 +25,48 @@ export default function Login() {
       <div className="login-form">
         <h2 className="title">账号登录</h2>
         {/* 失去焦点的时候以及改变的时候触发校验 */}
-        <Form validateTrigger={['onChange', 'onBlur']} onFinish={ onFinish}>
-          <Form.Item className="login-item" name="mobile" rules={[
-            {
-            required: true,
-            message:'手机号不能为空'
-            },
-            {
-              pattern: /^1[3456789]\d{9}$/,
-              message: '手机号格式不正确'
-            }
-          ]}>
-            <Input placeholder="请输入用户名" autoComplete='off'></Input>
+        <Form validateTrigger={['onChange', 'onBlur']} onFinish={onFinish}>
+          <Form.Item
+            initialValue="17634251475"
+            className="login-item"
+            name="mobile"
+            rules={[
+              {
+                required: true,
+                message: '手机号不能为空',
+              },
+              {
+                pattern: /^1[3456789]\d{9}$/,
+                message: '手机号格式不正确',
+              },
+            ]}
+          >
+            <Input placeholder="请输入用户名" autoComplete="off"></Input>
           </Form.Item>
           <List.Item
             className="login-code-extra"
             extra={<span className="code-extra">发送验证码</span>}
           >
-            <Form.Item className="login-item" name="code" rules={[
-              { required: true, message: '验证码不能为空' },
-              {pattern: /^\d{6}$/, message: '验证码格式不正确'}
-            ]}>
-              <Input placeholder="请输入验证码" autoComplete='off'></Input>
+            <Form.Item
+              initialValue="246810"
+              className="login-item"
+              name="code"
+              rules={[
+                { required: true, message: '验证码不能为空' },
+                { pattern: /^\d{6}$/, message: '验证码格式不正确' },
+              ]}
+            >
+              <Input placeholder="请输入验证码" autoComplete="off"></Input>
             </Form.Item>
           </List.Item>
           <Form.Item>
             {/* 提交表单校验 */}
-            <Button color="primary" type='submit' block className="login-submit">
+            <Button
+              color="primary"
+              type="submit"
+              block
+              className="login-submit"
+            >
               登录
             </Button>
           </Form.Item>
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,5 +1,6 @@
 // 封装axios
-import axios from 'axios'
+import { Toast } from 'antd-mobile'
+import axios, { AxiosError } from 'axios'
 
 const instance = axios.create({
   baseURL: 'http://geek.itheima.net/v1_0/',
@@ -24,7 +25,18 @@ instance.interceptors.response.use(
     // 对响应数据做点什么
     return response
   },
-  function (error) {
+  function (error: AxiosError<{ message: string }>) {
+    console.dir(error)
+    if (!error.response) {
+      // 由于网络繁忙导致请求失败
+      Toast.show('网络繁忙，请稍后再试')
+    } else {
+      // 不是网络繁忙导致请求失败
+      // console.log('我是错误');
+      console.log(error.response.data.message);
+      
+      Toast.show(error.response.data.message)
+    }
     // 对响应错误做点什么
     return Promise.reject(error)
   }

5114fa25985099c7b7a1551c8769ba961f8e0f88
水木年华 <2461981896@qq.com>
2022-04-19 16:39:52 +0800

    feat:频道弹层的显示和隐藏

---
+++ src/pages/Home/components/Channels/index.tsx
@@ -0,0 +1,45 @@
+import classnames from 'classnames'
+
+import Icon from '@/components/icon'
+import styles from './index.module.scss'
+type Props = {
+  hide: () => void
+}
+const Channels = ({ hide}:Props) => {
+  return (
+    <div className={styles.root}>
+      <div className="channel-header">
+        <Icon type="iconbtn_channel_close" onClick={hide}/>
+      </div>
+      <div className="channel-content">
+        {/* 编辑时，添加类名 edit */}
+        <div className={classnames('channel-item')}>
+          <div className="channel-item-header">
+            <span className="channel-item-title">我的频道</span>
+            <span className="channel-item-title-extra">点击进入频道</span>
+            <span className="channel-item-edit">编辑</span>
+          </div>
+          <div className="channel-list">
+            {/* 选中时，添加类名 selected */}
+            <span className={classnames('channel-list-item')}>
+              推荐
+              <Icon type="iconbtn_tag_close" />
+            </span>
+          </div>
+        </div>
+
+        <div className="channel-item">
+          <div className="channel-item-header">
+            <span className="channel-item-title">频道推荐</span>
+            <span className="channel-item-title-extra">点击添加频道</span>
+          </div>
+          <div className="channel-list">
+            <span className="channel-list-item">+ HTML</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Channels
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,21 @@
 import Icon from '@/components/icon'
 import { getUserChannel } from '@/store/actions/channel'
 import { useInitialState } from '@/utils/hooks'
-import { Tabs } from 'antd-mobile'
+import { Popup, Tabs } from 'antd-mobile'
+import { useState } from 'react'
+import Channels from './components/Channels'
 
 import styles from './index.module.scss'
 
 const Home = () => {
   const { userChannels } = useInitialState(getUserChannel, 'channel')
-
+  const [visible, setVisible] = useState(false)
+  const hide = () => { 
+    setVisible(false)
+  }
+  const show = () => { 
+    setVisible(true)
+  }
   return (
     <div className={styles.root}>
       {/* 频道 Tabs 列表 */}
@@ -23,8 +31,9 @@ const Home = () => {
 
       <div className="tabs-opration">
         <Icon type="iconbtn_search" />
-        <Icon type="iconbtn_channel" />
+        <Icon type="iconbtn_channel" onClick={show}/>
       </div>
+      <Popup position='left' visible={visible}><Channels hide={ hide}></Channels></Popup>
     </div>
   )
 }

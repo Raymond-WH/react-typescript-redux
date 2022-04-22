7d786d15a85f94aded872c090b5a7ee498d60ad0
水木年华 <2461981896@qq.com>
2022-04-22 18:17:01 +0800

    feat:搜索功能防抖ahooks useDeboundeFn

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -6,14 +6,20 @@ import Icon from '@/components/icon'
 import styles from './index.module.scss'
 // 导入useState
 import { useState } from 'react'
-
+import { useDebounceFn} from 'ahooks'
 const SearchPage = () => {
   const history = useHistory()
   // 使用useState定义搜索框的值
   const [value, setValue] = useState('')
+  const { run} = useDebounceFn(() => { 
+    console.log('需要搜索');
+    
+  })
   const onChange = (e: string) => {
     setValue(e)
     console.log('发送请求')
+    // 搜索功能需要防抖
+    run()
   }
 
   return (

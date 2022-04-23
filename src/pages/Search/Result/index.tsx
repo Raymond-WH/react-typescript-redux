d2a780cdc033c03048c5c5904a42d1faf99c579e
水木年华 <2461981896@qq.com>
2022-04-23 14:02:08 +0800

    feat:获取地址栏关键字keyword

--- src/pages/Search/Result/index.tsx
+++ src/pages/Search/Result/index.tsx
@@ -2,10 +2,18 @@ import { useHistory } from 'react-router-dom'
 import { NavBar } from 'antd-mobile'
 
 import styles from './index.module.scss'
-
+import qs from 'qs'
 const Result = () => {
   const history = useHistory()
+  // 获取地址栏的keyword参数
+  // const keyword = history.location.search.split('=')[1]
 
+  const res = qs.parse(history.location.search.slice(1))
+  console.log(res);
+  // ts内置的urlsearchparams
+  // const params = new URLSearchParams(history.location.search)
+  // const keyword = params.get('keyword')
+  
   return (
     <div className={styles.root}>
       <NavBar onBack={() => history.go(-1)}>搜索结果</NavBar>

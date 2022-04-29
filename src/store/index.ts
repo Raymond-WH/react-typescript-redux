ea7e19bb4ecb6e3a50696e74ecee2ca6830feed4
水木年华 <2461981896@qq.com>
2022-04-29 15:21:10 +0800

    feat:生产环境优化

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,7 +3,12 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import reducer from './reducers'
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
+// const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
 // console.log(composeWithDevTools(applyMiddleware(thunk)))
-
+const store = createStore(
+  reducer,
+  process.env.NODE_ENV === 'development'
+    ? composeWithDevTools(applyMiddleware(thunk))
+    : applyMiddleware(thunk)
+)
 export default store

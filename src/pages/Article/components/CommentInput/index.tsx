0aced5d91b78e4af97d3525ef40ff50dd7beb4b3
水木年华 <2461981896@qq.com>
2022-04-29 10:59:43 +0800

    feat:添加回复的弹层

--- src/pages/Article/components/CommentInput/index.tsx
+++ src/pages/Article/components/CommentInput/index.tsx
@@ -4,33 +4,43 @@ import { useState } from 'react'
 type Props = {
   // 评论的作者的名字
   name?: string
-  hideComment: () => void
-  addComment:(value:string)=>void
+  hideComment?: () => void
+  addComment?: (value: string) => void
 }
-export default function CommentInput({ name, hideComment,addComment }: Props) {
-  
+export default function CommentInput({ name, hideComment, addComment }: Props) {
   const [value, setValue] = useState('')
-  const add = () => { 
+  const add = () => {
     if (!value.trim()) return
-    addComment(value)
+    addComment?.(value)
     // 清空内容
-    
   }
   return (
     <div className={styles.root}>
-      <NavBar right={<span className="publish" onClick={add}>发表</span>} onBack={() => { 
-        hideComment()
-      }}>
+      <NavBar
+        right={
+          <span className="publish" onClick={add}>
+            发表
+          </span>
+        }
+        onBack={() => {
+          hideComment?.()
+        }}
+      >
         {name ? '回复评论' : '评论文章'}
       </NavBar>
       <div className="input-area">
         {/* 回复别人的评论时显示：@某某 */}
-        {name && <div className="at">@{name}:</div>}
+        {name && <div className="at"></div>}
 
         {/* 评论内容输入框 */}
-        <TextArea placeholder="说点什么~" rows={10} value={value} onChange={(e) => { 
-          setValue(e)
-        }}/>
+        <TextArea
+          placeholder={ `${name?`@${name}`:'说点什么'}` }
+          rows={10}
+          value={value}
+          onChange={(e) => {
+            setValue(e)
+          }}
+        />
       </div>
     </div>
   )
--- src/pages/Article/components/CommentReply/index.tsx
+++ src/pages/Article/components/CommentReply/index.tsx
@@ -1,4 +1,4 @@
-import { InfiniteScroll, NavBar } from 'antd-mobile'
+import { InfiniteScroll, NavBar, Popup } from 'antd-mobile'
 import CommentFooter from '../CommentFooter'
 import NoComment from '../NoComment'
 import styles from './index.module.scss'
@@ -6,6 +6,7 @@ import { Comment, CommentRes } from '@/types/data'
 import CommentItem from '../CommentItem'
 import { useState } from 'react'
 import { getReplyComments } from '@/store/actions/article'
+import CommentInput from '../CommentInput'
 type Props = {
   hideReply?: () => void
   comment?: Comment
@@ -15,27 +16,36 @@ export default function CommentReply({
   comment = {} as Comment,
 }: Props) {
   // 显示原始评论
-  console.log(comment);
-  
+  console.log(comment)
+
   // 显示评论回复
   // 获取评论的所有回复
   const [replyList, setReplyList] = useState<CommentRes>({
     results: [],
     total_count: 100,
     last_id: '',
-    end_id:''
+    end_id: '',
   })
 
   const hasMore = replyList.total_count > replyList.results.length
-  const loadMore = async () => { 
+  const loadMore = async () => {
     const res = await getReplyComments(comment.com_id, replyList.last_id)
     setReplyList({
       ...res.data.data,
       results: [...replyList.results, ...res.data.data.results],
-      total_count:res.data.data.total_count
+      total_count: res.data.data.total_count,
     })
-    // console.log('ddddd'); 
-    
+    // console.log('ddddd');
+  }
+  const [visible, seIsVisible] = useState(false)
+  const show = () => {
+    seIsVisible(true)
+  }
+  const hide = () => {
+    seIsVisible(false)
+  }
+  const addReply = (value:string) => { 
+
   }
   return (
     <div className={styles.root}>
@@ -70,7 +80,16 @@ export default function CommentReply({
         </div>
 
         {/* 评论工具栏，设置 type="reply" 不显示评论和点赞按钮 */}
-        <CommentFooter type='reply' />
+        <CommentFooter type="reply" showComment={show} />
+
+        {/* 添加回复功能 */}
+        <Popup position="bottom" visible={visible} destroyOnClose>
+          <CommentInput
+            name={comment.aut_name}
+            hideComment={hide}
+            addComment={ addReply}
+          ></CommentInput>
+        </Popup>
       </div>
     </div>
   )
